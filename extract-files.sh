LineageOS/android_device_asus_msm8916-common

msm8916-common: Update extract and makefiles scripts
* $CM_ROOT -> $LINEAGE_ROOT

extract-files.sh
@@ -22,9 +22,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -53,23 +53,23 @@ fi
 
 if [ -n "$SETUP" ]; then
     # Initialize the helper for common
-    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true false
+    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true false
     "$MY_DIR"/setup-makefiles.sh false
 
     if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
         # Initalize the helper for device
-        setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false false
+        setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false false
         "$MY_DIR"/setup-makefiles.sh false
     fi
 else
     # Initialize the helper for common
-    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true "$CLEANUP"
+    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true "$CLEANUP"
 
     extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
     if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
         # Reinitialize the helper for device
-        setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false "$CLEANUP"
+        setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEANUP"
 
         extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC"
     fi

setup-makefiles.sh
@@ -22,9 +22,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -32,7 +32,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper for common
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" "true" "$1"
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" "true" "$1"
 
 # Copyright headers and guards
 write_headers "Z00L Z00T"
@@ -45,7 +45,7 @@ write_footers
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" "false" "$1"
+    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" "false" "$1"
 
     # Copyright headers and guards
     write_headers

AospExtended-Devices/device_asus_msm8916-common

extract-files.sh
@@ -22,9 +22,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+AOSP_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$AOSP_ROOT"/vendor/aosp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -53,23 +53,23 @@ fi
 
 if [ -n "$SETUP" ]; then
     # Initialize the helper for common
-    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true false
+    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$AOSP_ROOT" true false
     "$MY_DIR"/setup-makefiles.sh false
 
     if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
         # Initalize the helper for device
-        setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false false
+        setup_vendor "$DEVICE" "$VENDOR" "$AOSP_ROOT" false false
         "$MY_DIR"/setup-makefiles.sh false
     fi
 else
     # Initialize the helper for common
-    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true "$CLEANUP"
+    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$AOSP_ROOT" true "$CLEANUP"
 
     extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
     if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
         # Reinitialize the helper for device
-        setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false "$CLEANUP"
+        setup_vendor "$DEVICE" "$VENDOR" "$AOSP_ROOT" false "$CLEANUP"
 
         extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC"
     fi

setup-makefiles.sh
@@ -22,9 +22,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+AOSP_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$AOSP_ROOT"/vendor/aosp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -32,7 +32,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper for common
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" "true" "$1"
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$AOSP_ROOT" "true" "$1"
 
 # Copyright headers and guards
 write_headers "Z00L Z00T"
@@ -45,7 +45,7 @@ write_footers
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" "false" "$1"
+    setup_vendor "$DEVICE" "$VENDOR" "$AOSP_ROOT" "false" "$1"
 
     # Copyright headers and guards
     write_headers