ArbabAliRajper/device_samsung_j5nlte

msm8916: init: Handle overriding system and vendor props
 * Cleanup

 * Up until now, vendor build properties were added into /system/build.prop
   when property split isn't enabled. However, Google realised that
   either /vendor/build.prop or /system/vendor/build.prop can always exist,
   and decided to clean up code and always install vendor build props
   into $(TARGET_OUT_VENDOR)/build.prop.

 * For this reason, the unified devices that used to override build props
   such as build fingerprint, will need to override the matching vendor
   build props.

Change-Id: Iacdd8eb67543daff5a46b92dbaf17cd094ce462b

init/init_msm8916.cpp
@@ -52,6 +52,12 @@ void property_override(char const prop[], char const value[])
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
+void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
 /* Read the file at filename and returns the integer
  * value in the file.
  *
@@ -150,13 +156,11 @@ void set_target_properties(const char *ro_build_id, const char *bootloader_str, 
 			name, device, version_release, build_id, bootloader);
 
 	/* set the build properties */
-	property_override("ro.bootimage.build.fingerprint", fingerprint);
 	property_override("ro.build.description", description);
 	property_override("ro.build.display.id", display_id);
-	property_override("ro.build.fingerprint", fingerprint);
-	property_override("ro.build.product", device);
-	property_override("ro.product.device", device);
-	property_override("ro.product.model", model);
+	property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", fingerprint);
+	property_override_dual("ro.product.device", "ro.vendor.product.model", device);
+	property_override_dual("ro.product.model", "ro.vendor.product.device", model);
 	android::init::property_set("ro.ril.telephony.mqanelements", "6");
 
 

AospExtended-Devices/device_asus_msm8916-common

 Conflicts:
	init/init_msm8916.cpp

init/init_msm8916.cpp
@@ -194,6 +194,12 @@ void property_override(char const prop[], char const value[])
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
+void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
 void vendor_load_properties()
 {
     char b_description[PROP_VALUE_MAX], b_fingerprint[PROP_VALUE_MAX];
@@ -212,12 +218,11 @@ void vendor_load_properties()
     sprintf(p_device, "ASUS_%s", device);
     sprintf(p_carrier, "US-ASUS_%s-WW_%s", device, device);
 
-    property_override("ro.build.product", family);
     property_override("ro.build.description", b_description);
-    property_override("ro.build.fingerprint", b_fingerprint);
     property_override("ro.product.carrier", p_carrier);
-    property_override("ro.product.device", p_device);
-    property_override("ro.product.model", p_model);
+	property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", b_fingerprint);
+	property_override_dual("ro.product.device", "ro.vendor.product.model", p_device);
+	property_override_dual("ro.product.model", "ro.vendor.product.device", p_model);
 
     property_set("dalvik.vm.heapstartsize", heapstartsize);
     property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);